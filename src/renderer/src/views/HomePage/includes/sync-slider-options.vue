99a7beb29673641b02873ce1219d191aa239c232
Marc Wenzelburger <marc.wenzelburger@pangoon.de>
2023-10-04 08:48:51 +0200

    Add - Pause Button To sync-slider-options.vue

--- src/renderer/src/views/HomePage/includes/sync-slider-options.vue
+++ src/renderer/src/views/HomePage/includes/sync-slider-options.vue
@@ -5,13 +5,19 @@
     <icon-button-component :icon="['fas', 'upload']"
                            emit-name="setSyncUpload"
                            @setSyncUpload="setSyncUpload"
-                           :btn-class="[uploadEnabled ? 'text-gray-200' : 'text-gray-800', 'w-[120px] transition flex justify-center items-center p-3']"
-                           :icon-class="[uploadEnabled ? 'text-gray-200' : 'text-gray-800', 'mr-2 transition']" >Upload</icon-button-component>
+                           :btn-class="[isActive('upload') ? 'text-gray-200' : 'text-gray-800', 'w-[120px] transition flex justify-center items-center p-3']"
+                           :icon-class="[isActive('upload') ? 'text-gray-200' : 'text-gray-800', 'mr-2 transition']" >Upload</icon-button-component>
     <icon-button-component :icon="['fas', 'download']"
                            emit-name="setSyncDownload"
                            @setSyncDownload="setSyncDownload"
-                           :btn-class="[uploadEnabled ? 'text-gray-800' : 'text-gray-200', 'w-[120px] transition flex justify-center items-center p-3']"
-                           :icon-class="[uploadEnabled ? 'text-gray-800' : 'text-gray-200', 'mr-2 transition']" >Download</icon-button-component>
+                           :btn-class="[isActive('download') ? 'text-gray-200' : 'text-gray-800', 'w-[120px] transition flex justify-center items-center p-3']"
+                           :icon-class="[isActive('download') ? 'text-gray-200' : 'text-gray-800', 'mr-2 transition']" >Download</icon-button-component>
+    <icon-button-component :icon="['fas', 'pause']"
+                           emit-name="setStopSyncingMethod"
+                           @setStopSyncingMethod="setStopSyncingMethod"
+                           :btn-class="[isActive('pause') ? 'text-gray-200' : 'text-gray-800', 'w-[120px] transition flex justify-center items-center p-3']"
+                           :icon-class="[isActive('pause') ? 'text-gray-200' : 'text-gray-800', 'mr-2 transition']" >Pause</icon-button-component>
+
   </div>
 </template>
 
@@ -35,22 +41,46 @@ const setSyncUpload = async() => {
 }
 
 
-const setSyncDownload = () => {
+const setSyncDownload = async() => {
   uploadEnabled.value = false;
-  stopSyncingMethod();
+  let clientSyncPath = await window.ipcRendererInvoke("get-setting", "clientSyncPath");
+  let ftpSyncPath = await window.ipcRendererInvoke('get-setting', 'ftp-sync-directory');
+
+  startSyncing('upload', clientSyncPath, ftpSyncPath)
+    .catch(error => {
+      console.error("Error in startSyncing:", error);
+
+    });
 
 }
 
-const stopSyncingMethod = () => {
+const setStopSyncingMethod = () => {
+  uploadEnabled.value = null;
   stopSyncing();
 }
 
+const isActive = (button) => {
+  if (button === 'upload') return uploadEnabled.value === true;
+  if (button === 'download') return uploadEnabled.value === false;
+  if (button === 'pause') return uploadEnabled.value === null;
+  return false;
+}
+
 const switchingBackgroundStyle = computed(() => {
-  return {
-    width: '120px',
-    transform: `translateX(${uploadEnabled.value ? -60 : 60}px)`,
-    transition: 'transform 0.3s ease-in-out'
-  };
+  if(uploadEnabled.value === null) {
+    return {
+      width: '120px',
+      transform: `translateX(120px)`,
+      transition: 'transform 0.3s ease-in-out'
+    };
+  }else {
+    return {
+      width: '120px',
+      transform: `translateX(${uploadEnabled.value ? -120 : 0}px)`,
+      transition: 'transform 0.3s ease-in-out'
+    };
+  }
+
 });
 </script>
 
