36bcd6c7f61ea315bddebf96ece48003a338022a
Marc Wenzelburger <marc.wenzelburger@pangoon.de>
2023-10-30 08:35:42 +0100

    Add - Better Check If Client And Ftp Sync Paths are valid Removed - Console Logs

--- src/main/ipcHandler.js
+++ src/main/ipcHandler.js
@@ -31,7 +31,6 @@ process.on('unhandledRejection', (reason, promise) => {
   console.error('Unhandled Rejection at:', promise, 'reason:', reason);
 });
 
-
 const store = new Store();
 //store.delete("firstStart");
 
